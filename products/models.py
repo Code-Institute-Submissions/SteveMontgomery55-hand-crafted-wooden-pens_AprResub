7fdf46682344232fd2301a71e23ef118b7f6d4af
SteveMontgomery55 <stevemontgomeryuk@yahoo.co.uk>
2022-01-28 22:44:01 +0000

    improve admin layout

--- products/models.py
+++ products/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
