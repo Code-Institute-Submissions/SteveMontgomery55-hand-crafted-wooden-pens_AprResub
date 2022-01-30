6932728fd3248c772a2abeaac812df464cdd1b26
SteveMontgomery55 <stevemontgomeryuk@yahoo.co.uk>
2022-01-30 17:06:52 +0000

    add products and search function

--- products/models.py
+++ products/models.py
@@ -5,8 +5,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
-class Category(models.Model):
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -22,7 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     woods = models.CharField(max_length=254, null=True, blank=True)
-    clips = models.CharField(max_length=254, null=True, blank=True)
+    clips = models.CharField(max_length=254, null=True, blank=True)    
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
