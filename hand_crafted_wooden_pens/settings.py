087f776e8b40e921f7e970aa87e50ad95f571d32
SteveMontgomery55 <stevemontgomeryuk@yahoo.co.uk>
2022-01-31 11:37:40 +0000

    add heroku

--- hand_crafted_wooden_pens/settings.py
+++ hand_crafted_wooden_pens/settings.py
@@ -108,12 +108,17 @@ WSGI_APPLICATION = 'hand_crafted_wooden_pens.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_url'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
