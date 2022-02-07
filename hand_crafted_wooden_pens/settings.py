c0bda63255b5c75386d579c07e3003528c86f286
SteveMontgomery55 <stevemontgomeryuk@yahoo.co.uk>
2022-02-07 13:05:26 +0000

    changed DATABASE_url to DATABASE_URL in settings.py

--- hand_crafted_wooden_pens/settings.py
+++ hand_crafted_wooden_pens/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'hand_crafted_wooden_pens.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_url'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
